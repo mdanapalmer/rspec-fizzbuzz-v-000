89ae071db96577cc726a99c8d83969a2cfb7e44e
Dana Palmer <roiai4@hotmail.com>
2018-09-28 22:27:24 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,9 @@
 def fizzbuzz(number)
-  if number % 3 == 0 && % 5 == 0
+  if number % 3 == 0 && number % 5 == 0
     "FizzBuzz"
-  elsif number % 5
+  elsif number % 5 == 0
     "Buzz"
-  elsif number % 3
+  elsif number % 3 == 0
     "Fizz"
   else
     nil
